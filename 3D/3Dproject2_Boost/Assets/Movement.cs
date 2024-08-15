f7a53b6bf01e777459ec6b24a4d18fa3989d2299
Lunamana00 <kth000928@naver.com>
2024-08-15 19:06:09 +0900

    proj2_movement

---
+++ 3D/3Dproject2_Boost/Assets/Movement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    float mainThrust = 1000f;
+    [SerializeField]
+    float rotateThrust = 100f;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ProcessTrust();
+        ProcessRotation();
+    }
+
+    void ProcessTrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRoataion(rotateThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRoataion(-rotateThrust);
+        }
+    }
+
+    void ApplyRoataion(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+    }
+}
